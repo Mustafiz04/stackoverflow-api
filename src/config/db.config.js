42a4bee99636a16d627ad6c64d3d949daca9f7d0
Mustafiz04 <kaifee000@gmail.com>
2024-04-27 20:13:16 +0530

    added apis

---
+++ src/config/db.config.js
@@ -0,0 +1,26 @@
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+
+const config = require('./index');
+
+dotenv.config();
+
+const sequelize = new Sequelize(config.DB.DATABASE, config.DB.USERNAME, config.DB.PASSWORD, {
+  dialect: 'mysql',
+  host: config.DB.HOST,
+  port: config.DB.PORT,
+  dialectOptions: {
+    connectTimeout: 30000,
+    timeout: 10000,
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000,
+    acquire: 30000,
+  },
+});
+
+(async () => await sequelize.sync())();
+
+module.exports = sequelize;
